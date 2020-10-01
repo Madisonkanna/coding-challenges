c6f5861a0fbe86d7190dc96320bc9fe244b6d87d
Jay Phelps <hello@jayphelps.com>
2020-09-30 21:54:31 -0400

    Fix counter to not be global variable

--- codewars/6kyu/Multiples-of-three-and-five/answer.js
+++ codewars/6kyu/Multiples-of-three-and-five/answer.js
@@ -4,7 +4,7 @@ function solution(number){
   //check if each number is a multiple of 3 or 5
   //add number to sum if it is
   let sum = 0
-  for (i = 0; i < number; i++) {
+  for (let i = 0; i < number; i++) {
     if (i % 3 === 0 || i % 5 === 0) {
       sum += i
     }
